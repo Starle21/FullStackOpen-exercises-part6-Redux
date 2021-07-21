e65ef6c251a422c5c991313779437e3181bca7cb
Starle21 <staronova.lenka@gmail.com>
2021-07-21 17:08:42 +0200

    Part 6.15 Anecdotes and backend, step3 - async dispatch initialize store

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes';
+
 // const anecdotesAtStart = [
 //   'If it hurts, do it more often',
 //   'Adding manpower to a late software project makes it later!',
@@ -58,10 +60,13 @@ export const giveVote = id => {
   return { type: 'UPVOTE', data: { id } };
 };
 
-export const initializeAnecdotes = anecdotes => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    });
   };
 };
 
